a28b75278a0da0c821a4b43cff3c3addc52bc064
嘉文 <DN4429@jsyyds.com>
2026-08-11 22:52:49 +0800

    [AI]配置读取nacos

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
 
+	"github.com/JarvanDante/my_ad/internal/boot"
 	"github.com/JarvanDante/my_ad/internal/dao"
 	"github.com/JarvanDante/my_ad/internal/modules/campaign"
 	campaignlogic "github.com/JarvanDante/my_ad/internal/modules/campaign/logic"
@@ -25,6 +26,7 @@ var Main = gcmd.Command{
 	Name:  "adapi",
 	Brief: "广告中台(PaaS) API",
 	Func: func(ctx context.Context, parser *gcmd.Parser) error {
+		boot.InitNacosConfig(ctx)
 		slotSvc := slotlogic.New(dao.NewSlotRepo())
 		creativeSvc := creativelogic.New(dao.NewCreativeRepo())
 		campaignSvc := campaignlogic.New(dao.NewCampaignRepo(), dao.NewSlotRepo(), dao.NewCreativeRepo())
