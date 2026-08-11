3baa9aabb69407a72e3f156c9e72a36e806e4457
嘉文 <DN4429@jsyyds.com>
2026-08-12 02:15:09 +0800

    [AI]feat: 接入 Nacos 配置源(env 引导 + 本地兜底)

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
 
-	"github.com/JarvanDante/my_ad/internal/boot"
 	"github.com/JarvanDante/my_ad/internal/dao"
 	"github.com/JarvanDante/my_ad/internal/modules/campaign"
 	campaignlogic "github.com/JarvanDante/my_ad/internal/modules/campaign/logic"
@@ -26,7 +25,6 @@ var Main = gcmd.Command{
 	Name:  "adapi",
 	Brief: "广告中台(PaaS) API",
 	Func: func(ctx context.Context, parser *gcmd.Parser) error {
-		boot.InitNacosConfig(ctx)
 		slotSvc := slotlogic.New(dao.NewSlotRepo())
 		creativeSvc := creativelogic.New(dao.NewCreativeRepo())
 		campaignSvc := campaignlogic.New(dao.NewCampaignRepo(), dao.NewSlotRepo(), dao.NewCreativeRepo())
